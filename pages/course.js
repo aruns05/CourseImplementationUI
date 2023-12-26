88aa5fd797510098e34fbf6472fda2b283a41e89
aruns05 <arun.s2k8@gmail.com>
2023-12-26 16:47:50 +0530

    Adding a UI Project

---
+++ pages/course.js
@@ -0,0 +1,22 @@
+import { Modal } from "@components/common";
+import {
+  CourseHero,
+  Curriculum,
+  Keypoints
+} from "@components/course";
+import { BaseLayout } from "@components/layout";
+
+export default function Course() {
+
+  return (
+    <>
+      <div className="py-4">
+        <CourseHero />
+      </div>
+      <Keypoints />
+      <Curriculum locked={true}/>
+      <Modal />
+    </>
+  )
+}
+Course.Layout = BaseLayout
